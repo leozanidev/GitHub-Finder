89428303a26fdd7f83e98847e271ec749635cd9c
Leonardo <leonardozanidev@gmail.com>
2025-11-26 10:02:34 -0300

    Done error component

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -4,13 +4,22 @@ import Search from "../components/Search";
 
 import { useState } from "react";
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     // Destructuring the upcome object
     const { avatar_url, login, location, followers, following } = data;
@@ -30,6 +39,7 @@ const Home = () => {
     <div>
       <Search loadUser={loadUser} />
       {user && <User {...user} />}
+      {error && <Error />}
     </div>
   );
 };
