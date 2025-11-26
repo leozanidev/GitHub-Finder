9c487788d80b201368ae9f33111986989201e4e7
Leonardo <leonardozanidev@gmail.com>
2025-11-26 09:04:45 -0300

    Initial router config

--- github_finder/src/App.tsx
+++ github_finder/src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import { Outlet } from "react-router-dom";
 
+function App() {
   return (
     <>
       <h1>GitHub Finder</h1>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- github_finder/src/main.tsx
+++ github_finder/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
