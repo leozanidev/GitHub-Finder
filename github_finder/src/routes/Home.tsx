45a5d262fba79b9f20a5081c3ef46e7f47e44c13
Leonardo <leonardozanidev@gmail.com>
2025-11-26 10:24:41 -0300

    done loader and user css

--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -5,16 +5,21 @@ import Search from "../components/Search";
 import { useState } from "react";
 import User from "../components/User";
 import Error from "../components/Error";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const loadUser = async (userName: string) => {
+    setIsLoading(true);
     setError(false);
     setUser(null);
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+
+    setIsLoading(false);
 
     if (res.status === 404) {
       setError(true);
@@ -38,6 +43,7 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser} />
+      {isLoading && <Loader />}
       {user && <User {...user} />}
       {error && <Error />}
     </div>
