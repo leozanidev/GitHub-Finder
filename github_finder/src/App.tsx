2af6fe813dbd9e01f0624309d9b48a49ac8852f2
Leonardo <leonardozanidev@gmail.com>
2025-11-26 09:33:31 -0300

    done css.module stylization on search and home pages

--- github_finder/src/App.tsx
+++ github_finder/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import classes from "./App.module.css";
+
 function App() {
   return (
     <>
-      <h1>GitHub Finder</h1>
-      <Outlet />
+      <div className={classes.app}>
+        <h1>GitHub Finder</h1>
+        <Outlet />
+      </div>
     </>
   );
 }
